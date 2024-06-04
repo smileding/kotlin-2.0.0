7868f855a23de5497f2c8acd349a0af1c6c8dc99
Valentin Slawicek <valentin.slawi@gmail.com>
2024-06-04 12:55:38 +0200

    Print errno for easier exec debugging

--- native/native.tests/testData/programName/main.c
+++ native/native.tests/testData/programName/main.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -9,6 +10,6 @@ int main(int argc, char *argv[]) {
     // Forward argv[2..n] to kotlin executable as arguments (the first one should be the programName according to posix)
     execv(argv[1], &(argv[2]));
 
-    printf("exec failed\n");
+    printf("exec failed with errno=%d\n", errno);
     return 1;
 }

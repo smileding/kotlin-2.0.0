478a555e92a7eb9fb6df039125f7d7744999e1c0
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2024-06-21 18:51:56 +0300

    [Wasm] Initial incremental compilation support for Wasm

--- compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
+++ compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/WasmLoweringPhases.kt
@@ -352,10 +352,10 @@ private val addContinuationToFunctionCallsLoweringPhase = makeIrModulePhase(
     )
 )
 
-private val addMainFunctionCallsLowering = makeIrModulePhase(
-    ::GenerateMainFunctionCalls,
-    name = "GenerateMainFunctionCalls",
-    description = "Generate main function calls into start function",
+private val generateMainFunctionWrappersPhase = makeIrModulePhase(
+    ::GenerateMainFunctionWrappers,
+    name = "GenerateMainFunctionWrappers",
+    description = "Find main functions and generate main function wrappers",
 )
 
 private val defaultArgumentStubGeneratorPhase = makeIrModulePhase<WasmBackendContext>(
@@ -684,7 +684,7 @@ val loweringList = listOf(
 
     wasmStringSwitchOptimizerLowering,
 
-    associatedObjectsLowering,
+//    associatedObjectsLowering,
 
     complexExternalDeclarationsToTopLevelFunctionsLowering,
     complexExternalDeclarationsUsagesLowering,
@@ -701,7 +701,7 @@ val loweringList = listOf(
 
     addContinuationToNonLocalSuspendFunctionsLoweringPhase,
     addContinuationToFunctionCallsLoweringPhase,
-    addMainFunctionCallsLowering,
+    generateMainFunctionWrappersPhase,
 
     invokeOnExportedFunctionExitLowering,
 
@@ -742,7 +742,7 @@ val loweringList = listOf(
 
     objectUsageLoweringPhase,
     purifyObjectInstanceGettersLoweringPhase,
-    fieldInitializersLoweringPhase,
+//    fieldInitializersLoweringPhase,
 
     explicitlyCastExternalTypesPhase,
     typeOperatorLoweringPhase,

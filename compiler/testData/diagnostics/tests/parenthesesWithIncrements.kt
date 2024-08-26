3903447b53824d9e749e274041ce4f42684f9f14
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-08-26 13:47:49 +0300

    [FIR] Reproduce current behavior of parentheses on assignments' LHS
    Note that the current K2 behavior is different from kotlin `2.0.20`,
    starting with `1760c705`. Before the commit:
    
    - `parenthesesWithAssignmentOrSetValCornerCases`:
      - `(b?.pa) += 10` and `(b?.ppa) += 10` - were green, now red
    - `parenthesesWithAssignmentOrSetVarCornerCases`
      - `(b?.p) += 10` and `(b?.pa) += 10` - were green, now red
    
    ^KT-70507
    ^KT-68834

---
+++ compiler/testData/diagnostics/tests/parenthesesWithIncrements.kt
@@ -0,0 +1,17 @@
+// FIR_IDENTICAL
+// ISSUE: KT-70507
+// WITH_STDLIB
+
+object O {
+    operator fun inc() = this
+
+    operator fun get(i: Int) = this
+    operator fun set(i: Int, o: O) {}
+}
+
+fun main() {
+    var b = O
+    (b)++
+
+    (O[1])++
+}

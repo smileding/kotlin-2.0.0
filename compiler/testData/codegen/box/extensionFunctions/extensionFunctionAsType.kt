7c142349efdfdeaba5bcc631328f4b55eeb4f502
Aleksandra.Arsenteva <aleksandra.arsenteva@jetbrains.com>
2024-09-03 09:57:19 +0200

    add first tests

---
+++ compiler/testData/codegen/box/extensionFunctions/extensionFunctionAsType.kt
@@ -0,0 +1,33 @@
+fun foo(x: Int.(y: String) -> Unit) {
+    1.x("")
+    x(1, "")
+}
+
+fun bar(): Int.(y: String) -> Unit {
+    return { y: String ->  }
+}
+val a: Int.(y: String) -> Unit = {}
+
+class B(foo: Int.(y: String) -> Unit) {
+    init {
+        4.foo("")
+    }
+}
+
+fun test(){
+    foo { y: String -> y.length }
+
+    val t = bar()
+    1.t("")
+    t(1, "")
+
+    1.a("")
+    a(1, "")
+
+    B { y: String -> y.length }
+}
+
+fun box(): String {
+    //test()
+    return "OK"
+}

alkaid616/kotlin

[FIR] Disable assignment and `set` resolution for parenthesized LHS
This change implements the prohibition by disabling resolution to the
set operator altogether. While this sounds handy for performance
reasons, it leads to some errors being replaced. It's not a big deal,
since its red->red.

The changes in `FirReassignmentAndInvisibleSetterChecker` and
`FirExpressionUtil` prevent a false-positive `VARIABLE_EXPECTED` on
parenthesized variable accesses.

`CANNOT_INFER_PARAMETER_TYPE` appeared in `testListPlusAssign`, because
previously the call would have been resolved to `plus()`, but we are now
left with the unsuccessful `plusAssign()`. For the same reason some
`VAL_REASSIGNMENT` and `VARIABLE_EXPECTED` may disappear and be replaced
with the deprecation warning (see `LValueAssignment.latestLV.kt`).

^KT-70507 Fixed

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDiagnostics.kt
@@ -254,6 +254,14 @@ sealed interface KaFirDiagnostic<PSI : PsiElement> : KaDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = RedundantInterpolationPrefix::class
     }
 
+    interface ParenthesizedLhsError : KaFirDiagnostic<PsiElement> {
+        override val diagnosticClass get() = ParenthesizedLhsError::class
+    }
+
+    interface ParenthesizedLhsWarning : KaFirDiagnostic<PsiElement> {
+        override val diagnosticClass get() = ParenthesizedLhsWarning::class
+    }
+
     interface InvisibleReference : KaFirDiagnostic<PsiElement> {
         override val diagnosticClass get() = InvisibleReference::class
         val reference: KaSymbol

smileding/kotlin-2.0.0

analysis/analysis-api-fir/src/org/jetbrains/kotlin/analysis/api/fir/diagnostics/KaFirDiagnostics.kt
@@ -254,6 +254,14 @@ sealed interface KaFirDiagnostic<PSI : PsiElement> : KaDiagnosticWithPsi<PSI> {
         override val diagnosticClass get() = RedundantInterpolationPrefix::class
     }
 
+    interface WrappedLhsInAssignmentError : KaFirDiagnostic<PsiElement> {
+        override val diagnosticClass get() = WrappedLhsInAssignmentError::class
+    }
+
+    interface WrappedLhsInAssignmentWarning : KaFirDiagnostic<PsiElement> {
+        override val diagnosticClass get() = WrappedLhsInAssignmentWarning::class
+    }
+
     interface InvisibleReference : KaFirDiagnostic<PsiElement> {
         override val diagnosticClass get() = InvisibleReference::class
         val reference: KaSymbol
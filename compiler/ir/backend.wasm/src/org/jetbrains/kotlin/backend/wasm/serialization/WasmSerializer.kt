vickyleu/kotlin

[Wasm] Complete WasmSerializer and WasmDeserializer

compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/serialization/WasmSerializer.kt
@@ -16,28 +16,40 @@ import java.util.*
 
 /**
  * Each polymorphic type is prepended by a type id byte, so that we know the type of the object to
- *  construct on deserialization.
+ *  construct on deserialization. To prepend a serialization by an id, a [withId] block is used.
  *
  * All booleans and flags (whether a nullable field is null or not) are combined into a byte bitset.
  *  This bitset (if exists) is prepended to the struct. The interpretation of each bit of the bitset
  *  is up to each function implementation. One exception is when there is only a single boolean field,
- *  in which case, it can be anywhere.
+ *  in which case, it can be anywhere. To prepend a serialization by a bitset, a [withFlags] block is used.
  *
- * When deserializing, a [WasmSymbol] referenced by multiple object (serialize multiple times) must
- *  deserialize to the same instance everywhere. For this to be possible, symbols are stored at a
- *  symbol table at the front. When a symbol is to be serialized, it's substituted by an integer
- *  that indicates its position in the table. This integer is used as a reference to the symbol.
+ * Some objects need to be serialized as references, meaning that if the same instance is serialized
+ *  in more than one place, all deserialized instances must be the same in all of these locations.
+ *  [WasmSymbol] is the most obvious example for instances that must be serialized by reference, but
+ *  is not the only object type. To change a serialization to be by reference instead of by value,
+ *  all you need is to put the serialization code inside a [serializeAsReference] block.
  *
- * The structure of the serialized symbol table is as follows:
+ * For clarity, we'll use [WasmSymbol] as an example when discussing the reference table.
+ * When deserializing, a [WasmSymbol] referenced by multiple object (serialized multiple times)
+ *  all deserialized instances of this symbol must be the same instance. For this to be possible,
+ *  symbols are stored in a reference table at the front of the serialized output. When a symbol
+ *  is to be serialized, it's substituted by an integer that indicates its position in the table.
+ *  This integer is used as a reference to the symbol. If the same symbol is referenced at two
+ *  locations, the serialization output will contain the same integer in place of the two symbol
+ *  references, thus, pointing to the same location in the reference table.
+ *
+ * The structure of the serialized reference table is as follows:
  *  N n0 d0 n1 d1 n2 d2... where:
  *    N : number of elements in the table
  *    nx: size (in bytes) of the table slot for element x
  *    dx: actual data of element x
- *  We need to store the size of each element because symbol sizes are not the same.
+ *  We need to store the size of each element because element sizes are not the same.
  *
- * When deserializing a symbol, the type of the data in each slot is determined by the context.
+ * The reference table can contain any object type, and not limited to a set of types.
+ *
+ * When deserializing a reference, the type of the data in each slot is determined by the context.
  *  For example, if we're deserializing a class `X` that contains a `WasmSymbol<Y>`, and the
- *  symbol reference for this symbol is 3, then we know that slot 3 in the index table contains
+ *  reference for this symbol is 3, then we know that slot 3 in the index table contains
  *  a `WasmSymbol<Y>`, and this is how we're going to interpret the bytes in slot 3.
  *
  * See [WasmDeserializer]
@@ -46,29 +58,43 @@ import java.util.*
 class WasmSerializer(outputStream: OutputStream) {
 
     /**
-     * @property id Unique identifier for the symbol that is used as an index in the symbol table
-     * @property serializeFunc When serializeFunc is executed, a symbol it holds is serialized
+     * @property id Unique identifier for the reference that is used as an index in the reference table
+     * @property serializeFunc Contains the serialization logic for the desired object
      */
-    private data class SymbolInfo(val id: Int, val serializeFunc: () -> Unit)
+    private data class DeferredSerialization(val id: Int, val serializeFunc: () -> Unit)
 
     /**
-     * Used to make sure that if a symbol is already serialized, it doesn't get serialized again.
-     *  This happens by assigning each symbol a unique id (an index into a table of symbols). This
-     *  id is used as a reference to the symbol.
+     * Used to make sure that if a reference is already serialized, it doesn't get serialized again.
+     *  This happens by assigning each reference a unique id (an index into the reference table). This
+     *  id is used as a reference to the object in the reference table.
      *
-     * When serializing a symbol, the symbol is written in the table, and is substituted by its id.
+     * When serializing a reference, the object is serialized into the table, and is substituted by its id.
      *
-     * This will be used once all structs are processed, and the number of unique symbols are known.
-     *  Functions here will, execute first, then the body of the file is appended to the header.
+     * This will be used once all objects are processed, and the number of references are known.
+     *  After holding the content of the serialization body in the [bodyBuffer] temporarily, the
+     *  functions in [referenceTable] is executed one by one, ordered by the id, effectively,
+     *  serializing the content of the reference table first, then the content in [bodyBuffer]
+     *  is output after the reference table content.
      *
      * This map must compare REFERENCES, and not values, two different symbol instances should serialize
      *  twice even if they're equal in value, but the same instance serialized twice should serialize only once.
      */
-    private val symbolTable: IdentityHashMap<WasmSymbolReadOnly<*>, SymbolInfo> = IdentityHashMap()
+    private val referenceTable: IdentityHashMap<Any, DeferredSerialization> = IdentityHashMap()
+
+    /**
+     * When executing serialization functions in [referenceTable], these functions may introduce new
+     *  references that need to be added to the reference table. To process all references, this list
+     *  accumulates references added to the [referenceTable], copied (sorted), cleared, then functions
+     *  in this list are executed. This process repeats while [newReferences] is not empty.
+     *
+     * Note that we can't clear the [referenceTable] instead, because this holds all references and is
+     *  used as a lookup table as well to avoid serializing the same instance more than one.
+     */
+    private val newReferences = mutableListOf<DeferredSerialization>()
 
     /**
      * Body of the serialized file. It's kept in a buffer (not written directly), because we'll
-     *  need to defer writing the body until the table of symbols is created and written.
+     *  need to defer writing the body until the reference table is created and written.
      */
     private val bodyBuffer = ByteArrayOutputStream()
     private val body = ByteWriter.OutputStream(bodyBuffer)
@@ -76,7 +102,7 @@ class WasmSerializer(outputStream: OutputStream) {
     /**
      * Temporary buffer, mainly used to store content that is prepended by its size, but the
      *  size is not known in advance. The output is written in this temp buffer, then its
-     *  size is written, then the content in the content in the temp buffer is written.
+     *  size is written, then the content in the temp buffer is written.
      *
      * We need reference to the tempBuffer as a [ByteArrayOutputStream] to be able to
      *  reset its content manually.
@@ -88,40 +114,48 @@ class WasmSerializer(outputStream: OutputStream) {
      * The current serialization target. This can be set to differently according to the current
      *  stage in the serialization process.
      *
-     * This is set to `body` by default, because this is the first target bytes will be written to.
+     * This is set to `body` by default, because this is the first target the bytes will be written to.
      */
     private var b: ByteWriter = body
 
     private val out = ByteWriter.OutputStream(outputStream)
 
     fun serialize(compiledFileFragment: WasmCompiledFileFragment) {
-        // Step 1: process all content
+        // Step 1: process non-deferred serializations (put into bodyBuffer temporarily)
         serializeCompiledFileFragment(compiledFileFragment)
 
-        // Step 2: output symbol table first
-        // The table is written at the beginning of the output
-        // Step 2.1: output size
-        if (symbolTable.size > 65535) {
-            error("The symbol table size ${symbolTable.size} (and ids) doesn't fit in a 16-bit integer. Consider using 32-bit integers.")
-        }
-        out.writeUInt16(symbolTable.size.toUShort())
+        // Step 2: output the reference table first, in the form: size content
+        // Step 2.1: output each element in the form: sizeInBytes data
 
-        // Step 2.2: output each element in the form: sizeInBytes data
+        // Compute the bytes of each table slot
         b = temp
-        symbolTable.values.sortedBy { it.id }.forEach {
-            tempBuffer.reset()
-            it.serializeFunc()
-
-            // Write the size
-            val size = tempBuffer.size()
-            if (size > 65535) error("The symbol size $size can't fit in a 16-bit integer. Consider using 32-bit integers.")
-            out.writeUInt16(size.toUShort())
-
-            // Write the data, which is currently in the temp buffer.
-            out.writeBytes(tempBuffer.toByteArray())
+        val tableElementsInBytes = mutableListOf<ByteArray>()
+        while (newReferences.isNotEmpty()) {
+            val sorted = newReferences.sortedBy { it.id }
+            newReferences.clear()
+            sorted.forEach {
+                tempBuffer.reset()
+                it.serializeFunc()
+                tableElementsInBytes.add(tempBuffer.toByteArray())
+            }
         }
 
-        // Step 3: after the symbol table is written, write the body
+        // Output the size of the reference table first
+        val size = tableElementsInBytes.size
+        if (size > 65535) {
+            error("The symbol table size $size (and ids) doesn't fit in a 16-bit integer. Consider using 32-bit integers.")
+        }
+        out.writeUInt16(size.toUShort())
+
+        // Output each element in the form: sizeInBytes data
+        tableElementsInBytes.forEach {
+            val bytesCount = it.size
+            if (bytesCount > 65535) error("The symbol size $bytesCount can't fit in a 16-bit integer. Consider using 32-bit integers.")
+            out.writeUInt16(bytesCount.toUShort())
+            out.writeBytes(it)
+        }
+
+        // Step 3: after the reference table is written, write the body
         out.writeBytes(bodyBuffer.toByteArray())
     }
 
@@ -383,7 +417,7 @@ class WasmSerializer(outputStream: OutputStream) {
             }
         }
 
-    private fun serialize(idSignature: IdSignature) {
+    private fun serialize(idSignature: IdSignature) =
         when (idSignature) {
             is IdSignature.AccessorSignature -> withId(0U) { serialize(idSignature) }
             is IdSignature.CommonSignature -> withId(1U) { serialize(idSignature) }
@@ -393,9 +427,8 @@ class WasmSerializer(outputStream: OutputStream) {
             is IdSignature.LoweredDeclarationSignature -> withId(5U) { serialize(idSignature) }
             is IdSignature.ScopeLocalDeclaration -> withId(6U) { serialize(idSignature) }
             is IdSignature.SpecialFakeOverrideSignature -> withId(7U) { serialize(idSignature) }
-            is IdSignature.FileSignature -> TODO()
+            is IdSignature.FileSignature -> withId(8U) { }
         }
-    }
 
     private fun serialize(accessor: IdSignature.AccessorSignature) {
         with(accessor) {
@@ -550,18 +583,12 @@ class WasmSerializer(outputStream: OutputStream) {
         serialize(wasmToIr, wasmSerializeFunc, irSerializeFunc)
     }
 
-    private fun <T : Any> serialize(symbol: WasmSymbolReadOnly<T>, serializeFunc: (T) -> Unit) {
-        val func = {
+    private fun <T : Any> serialize(symbol: WasmSymbolReadOnly<T>, serializeFunc: (T) -> Unit) =
+        serializeAsReference(symbol) {
             withFlags(symbol.getOwner() == null) {
                 symbol.getOwner()?.let { serializeFunc(it) }
             }
         }
-        val info = symbolTable.getOrPut(symbol) {
-            SymbolInfo(symbolTable.size, func)
-        }
-        assert(info.id <= 65535)
-        b.writeUInt16(info.id.toUShort())
-    }
 
     private fun serializeCompiledFileFragment(compiledFileFragment: WasmCompiledFileFragment) =
         with(compiledFileFragment) {
@@ -594,11 +621,13 @@ class WasmSerializer(outputStream: OutputStream) {
         }
 
     private fun serializeNamedModuleField(obj: WasmNamedModuleField, flags: List<Boolean> = listOf(), serializeFunc: () -> Unit) =
-        // Serializes the common part of WasmNamedModuleField.
-        withFlags(*listOf(obj.id == null, obj.name.isEmpty()).plus(flags).toBooleanArray()) {
-            obj.id?.let { b.writeUInt32(it.toUInt()) }
-            if (obj.name.isNotEmpty()) serialize(obj.name)
-            serializeFunc()
+        serializeAsReference(obj) {
+            // Serializes the common part of WasmNamedModuleField.
+            withFlags(*listOf(obj.id == null, obj.name.isEmpty()).plus(flags).toBooleanArray()) {
+                obj.id?.let { b.writeUInt32(it.toUInt()) }
+                if (obj.name.isNotEmpty()) serialize(obj.name)
+                serializeFunc()
+            }
         }
 
     private fun withIdNullable(id: UByte, obj: Any?, serializeFunc: () -> Unit) {
@@ -620,6 +649,17 @@ class WasmSerializer(outputStream: OutputStream) {
         }
         b.writeUByte(flagsToUByte(flags))
         serializeFunc()
+    }
+
+    private fun serializeAsReference(obj: Any, serializeFunc: () -> Unit) {
+        val id = referenceTable.getOrElse(obj) {
+            DeferredSerialization(referenceTable.size, serializeFunc).also {
+                referenceTable[obj] = it
+                newReferences.add(it)
+            }
+        }.id
+        assert(id <= 65535)
+        b.writeUInt16(id.toUShort())
     }
 
     private fun <T : Any> WasmSymbolReadOnly<T>.getOwner() =

smileding/kotlin-2.0.0

compiler/ir/backend.wasm/src/org/jetbrains/kotlin/backend/wasm/serialization/WasmSerializer.kt
@@ -16,28 +16,40 @@ import java.util.*
 
 /**
  * Each polymorphic type is prepended by a type id byte, so that we know the type of the object to
- *  construct on deserialization.
+ *  construct on deserialization. To prepend a serialization by an id, a [withId] block is used.
  *
  * All booleans and flags (whether a nullable field is null or not) are combined into a byte bitset.
  *  This bitset (if exists) is prepended to the struct. The interpretation of each bit of the bitset
  *  is up to each function implementation. One exception is when there is only a single boolean field,
- *  in which case, it can be anywhere.
+ *  in which case, it can be anywhere. To prepend a serialization by a bitset, a [withFlags] block is used.
  *
- * When deserializing, a [WasmSymbol] referenced by multiple object (serialize multiple times) must
- *  deserialize to the same instance everywhere. For this to be possible, symbols are stored at a
- *  symbol table at the front. When a symbol is to be serialized, it's substituted by an integer
- *  that indicates its position in the table. This integer is used as a reference to the symbol.
+ * Some objects need to be serialized as references, meaning that if the same instance is serialized
+ *  in more than one place, all deserialized instances must be the same in all of these locations.
+ *  [WasmSymbol] is the most obvious example for instances that must be serialized by reference, but
+ *  is not the only object type. To change a serialization to be by reference instead of by value,
+ *  all you need is to put the serialization code inside a [serializeAsReference] block.
  *
- * The structure of the serialized symbol table is as follows:
+ * For clarity, we'll use [WasmSymbol] as an example when discussing the reference table.
+ * When deserializing, a [WasmSymbol] referenced by multiple object (serialized multiple times)
+ *  all deserialized instances of this symbol must be the same instance. For this to be possible,
+ *  symbols are stored in a reference table at the front of the serialized output. When a symbol
+ *  is to be serialized, it's substituted by an integer that indicates its position in the table.
+ *  This integer is used as a reference to the symbol. If the same symbol is referenced at two
+ *  locations, the serialization output will contain the same integer in place of the two symbol
+ *  references, thus, pointing to the same location in the reference table.
+ *
+ * The structure of the serialized reference table is as follows:
  *  N n0 d0 n1 d1 n2 d2... where:
  *    N : number of elements in the table
  *    nx: size (in bytes) of the table slot for element x
  *    dx: actual data of element x
- *  We need to store the size of each element because symbol sizes are not the same.
+ *  We need to store the size of each element because element sizes are not the same.
  *
- * When deserializing a symbol, the type of the data in each slot is determined by the context.
+ * The reference table can contain any object type, and not limited to a set of types.
+ *
+ * When deserializing a reference, the type of the data in each slot is determined by the context.
  *  For example, if we're deserializing a class `X` that contains a `WasmSymbol<Y>`, and the
- *  symbol reference for this symbol is 3, then we know that slot 3 in the index table contains
+ *  reference for this symbol is 3, then we know that slot 3 in the index table contains
  *  a `WasmSymbol<Y>`, and this is how we're going to interpret the bytes in slot 3.
  *
  * See [WasmDeserializer]
@@ -46,29 +58,43 @@ import java.util.*
 class WasmSerializer(outputStream: OutputStream) {
 
     /**
-     * @property id Unique identifier for the symbol that is used as an index in the symbol table
-     * @property serializeFunc When serializeFunc is executed, a symbol it holds is serialized
+     * @property id Unique identifier for the reference that is used as an index in the reference table
+     * @property serializeFunc Contains the serialization logic for the desired object
      */
-    private data class SymbolInfo(val id: Int, val serializeFunc: () -> Unit)
+    private data class DeferredSerialization(val id: Int, val serializeFunc: () -> Unit)
 
     /**
-     * Used to make sure that if a symbol is already serialized, it doesn't get serialized again.
-     *  This happens by assigning each symbol a unique id (an index into a table of symbols). This
-     *  id is used as a reference to the symbol.
+     * Used to make sure that if a reference is already serialized, it doesn't get serialized again.
+     *  This happens by assigning each reference a unique id (an index into the reference table). This
+     *  id is used as a reference to the object in the reference table.
      *
-     * When serializing a symbol, the symbol is written in the table, and is substituted by its id.
+     * When serializing a reference, the object is serialized into the table, and is substituted by its id.
      *
-     * This will be used once all structs are processed, and the number of unique symbols are known.
-     *  Functions here will, execute first, then the body of the file is appended to the header.
+     * This will be used once all objects are processed, and the number of references are known.
+     *  After holding the content of the serialization body in the [bodyBuffer] temporarily, the
+     *  functions in [referenceTable] is executed one by one, ordered by the id, effectively,
+     *  serializing the content of the reference table first, then the content in [bodyBuffer]
+     *  is output after the reference table content.
      *
      * This map must compare REFERENCES, and not values, two different symbol instances should serialize
      *  twice even if they're equal in value, but the same instance serialized twice should serialize only once.
      */
-    private val symbolTable: IdentityHashMap<WasmSymbolReadOnly<*>, SymbolInfo> = IdentityHashMap()
+    private val referenceTable: IdentityHashMap<Any, DeferredSerialization> = IdentityHashMap()
+
+    /**
+     * When executing serialization functions in [referenceTable], these functions may introduce new
+     *  references that need to be added to the reference table. To process all references, this list
+     *  accumulates references added to the [referenceTable], copied (sorted), cleared, then functions
+     *  in this list are executed. This process repeats while [newReferences] is not empty.
+     *
+     * Note that we can't clear the [referenceTable] instead, because this holds all references and is
+     *  used as a lookup table as well to avoid serializing the same instance more than one.
+     */
+    private val newReferences = mutableListOf<DeferredSerialization>()
 
     /**
      * Body of the serialized file. It's kept in a buffer (not written directly), because we'll
-     *  need to defer writing the body until the table of symbols is created and written.
+     *  need to defer writing the body until the reference table is created and written.
      */
     private val bodyBuffer = ByteArrayOutputStream()
     private val body = ByteWriter.OutputStream(bodyBuffer)
@@ -76,7 +102,7 @@ class WasmSerializer(outputStream: OutputStream) {
     /**
      * Temporary buffer, mainly used to store content that is prepended by its size, but the
      *  size is not known in advance. The output is written in this temp buffer, then its
-     *  size is written, then the content in the content in the temp buffer is written.
+     *  size is written, then the content in the temp buffer is written.
      *
      * We need reference to the tempBuffer as a [ByteArrayOutputStream] to be able to
      *  reset its content manually.
@@ -88,40 +114,48 @@ class WasmSerializer(outputStream: OutputStream) {
      * The current serialization target. This can be set to differently according to the current
      *  stage in the serialization process.
      *
-     * This is set to `body` by default, because this is the first target bytes will be written to.
+     * This is set to `body` by default, because this is the first target the bytes will be written to.
      */
     private var b: ByteWriter = body
 
     private val out = ByteWriter.OutputStream(outputStream)
 
     fun serialize(compiledFileFragment: WasmCompiledFileFragment) {
-        // Step 1: process all content
+        // Step 1: process non-deferred serializations (put into bodyBuffer temporarily)
         serializeCompiledFileFragment(compiledFileFragment)
 
-        // Step 2: output symbol table first
-        // The table is written at the beginning of the output
-        // Step 2.1: output size
-        if (symbolTable.size > 65535) {
-            error("The symbol table size ${symbolTable.size} (and ids) doesn't fit in a 16-bit integer. Consider using 32-bit integers.")
-        }
-        out.writeUInt16(symbolTable.size.toUShort())
+        // Step 2: output the reference table first, in the form: size content
+        // Step 2.1: output each element in the form: sizeInBytes data
 
-        // Step 2.2: output each element in the form: sizeInBytes data
+        // Compute the bytes of each table slot
         b = temp
-        symbolTable.values.sortedBy { it.id }.forEach {
-            tempBuffer.reset()
-            it.serializeFunc()
-
-            // Write the size
-            val size = tempBuffer.size()
-            if (size > 65535) error("The symbol size $size can't fit in a 16-bit integer. Consider using 32-bit integers.")
-            out.writeUInt16(size.toUShort())
-
-            // Write the data, which is currently in the temp buffer.
-            out.writeBytes(tempBuffer.toByteArray())
+        val tableElementsInBytes = mutableListOf<ByteArray>()
+        while (newReferences.isNotEmpty()) {
+            val sorted = newReferences.sortedBy { it.id }
+            newReferences.clear()
+            sorted.forEach {
+                tempBuffer.reset()
+                it.serializeFunc()
+                tableElementsInBytes.add(tempBuffer.toByteArray())
+            }
         }
 
-        // Step 3: after the symbol table is written, write the body
+        // Output the size of the reference table first
+        val size = tableElementsInBytes.size
+        if (size > 65535) {
+            error("The symbol table size $size (and ids) doesn't fit in a 16-bit integer. Consider using 32-bit integers.")
+        }
+        out.writeUInt16(size.toUShort())
+
+        // Output each element in the form: sizeInBytes data
+        tableElementsInBytes.forEach {
+            val bytesCount = it.size
+            if (bytesCount > 65535) error("The symbol size $bytesCount can't fit in a 16-bit integer. Consider using 32-bit integers.")
+            out.writeUInt16(bytesCount.toUShort())
+            out.writeBytes(it)
+        }
+
+        // Step 3: after the reference table is written, write the body
         out.writeBytes(bodyBuffer.toByteArray())
     }
 
@@ -385,7 +419,7 @@ class WasmSerializer(outputStream: OutputStream) {
             }
         }
 
-    private fun serialize(idSignature: IdSignature) {
+    private fun serialize(idSignature: IdSignature) =
         when (idSignature) {
             is IdSignature.AccessorSignature -> withId(0U) { serialize(idSignature) }
             is IdSignature.CommonSignature -> withId(1U) { serialize(idSignature) }
@@ -395,9 +429,8 @@ class WasmSerializer(outputStream: OutputStream) {
             is IdSignature.LoweredDeclarationSignature -> withId(5U) { serialize(idSignature) }
             is IdSignature.ScopeLocalDeclaration -> withId(6U) { serialize(idSignature) }
             is IdSignature.SpecialFakeOverrideSignature -> withId(7U) { serialize(idSignature) }
-            is IdSignature.FileSignature -> TODO()
+            is IdSignature.FileSignature -> withId(8U) { }
         }
-    }
 
     private fun serialize(accessor: IdSignature.AccessorSignature) {
         with(accessor) {
@@ -552,18 +585,12 @@ class WasmSerializer(outputStream: OutputStream) {
         serialize(wasmToIr, wasmSerializeFunc, irSerializeFunc)
     }
 
-    private fun <T : Any> serialize(symbol: WasmSymbolReadOnly<T>, serializeFunc: (T) -> Unit) {
-        val func = {
+    private fun <T : Any> serialize(symbol: WasmSymbolReadOnly<T>, serializeFunc: (T) -> Unit) =
+        serializeAsReference(symbol) {
             withFlags(symbol.getOwner() == null) {
                 symbol.getOwner()?.let { serializeFunc(it) }
             }
         }
-        val info = symbolTable.getOrPut(symbol) {
-            SymbolInfo(symbolTable.size, func)
-        }
-        assert(info.id <= 65535)
-        b.writeUInt16(info.id.toUShort())
-    }
 
     private fun serializeCompiledFileFragment(compiledFileFragment: WasmCompiledFileFragment) =
         with(compiledFileFragment) {
@@ -596,11 +623,13 @@ class WasmSerializer(outputStream: OutputStream) {
         }
 
     private fun serializeNamedModuleField(obj: WasmNamedModuleField, flags: List<Boolean> = listOf(), serializeFunc: () -> Unit) =
-        // Serializes the common part of WasmNamedModuleField.
-        withFlags(*listOf(obj.id == null, obj.name.isEmpty()).plus(flags).toBooleanArray()) {
-            obj.id?.let { b.writeUInt32(it.toUInt()) }
-            if (obj.name.isNotEmpty()) serialize(obj.name)
-            serializeFunc()
+        serializeAsReference(obj) {
+            // Serializes the common part of WasmNamedModuleField.
+            withFlags(*listOf(obj.id == null, obj.name.isEmpty()).plus(flags).toBooleanArray()) {
+                obj.id?.let { b.writeUInt32(it.toUInt()) }
+                if (obj.name.isNotEmpty()) serialize(obj.name)
+                serializeFunc()
+            }
         }
 
     private fun withIdNullable(id: UByte, obj: Any?, serializeFunc: () -> Unit) {
@@ -622,6 +651,16 @@ class WasmSerializer(outputStream: OutputStream) {
         }
         b.writeUByte(flagsToUByte(flags))
         serializeFunc()
+    }
+
+    private fun serializeAsReference(obj: Any, serializeFunc: () -> Unit) {
+        val id = referenceTable.getOrPut(obj) {
+            DeferredSerialization(referenceTable.size, serializeFunc).also {
+                newReferences.add(it)
+            }
+        }.id
+        assert(id <= 65535)
+        b.writeUInt16(id.toUShort())
     }
 
     private fun <T : Any> WasmSymbolReadOnly<T>.getOwner() =